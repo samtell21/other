94864cfaf4d83e3f5bbacd7bb79c81bda6795bc4
samtell21 <samtell21@gmail.com>
2020-06-04 15:20:41 +0000

    crackle...POP

--- cracklepop_new.rb
+++ cracklepop_new.rb
@@ -1,19 +1,26 @@
 module SwapOut
     refine Enumerable do
-        def swap(arr)
-            map.with_index{|e, i| arr[i] || e}
-        end
-        def append_or_swap(arr)
-            map.with_index{|e, i| (e+arr[i] rescue arr[i]) || e}
+        def swap(hash)
+            map{|e| hash[e] || e}
         end
     end
 end
+
+module SemiWeakTyping
+    refine Object do
+        def add_or_return x
+            self+x rescue x
+        end
+    end
+end
+
 using SwapOut
+using SemiWeakTyping
 def cracklepop
-    #taking advantage of some special syntax & duck typing that lets me use a block like a dynamically computed array
-    crackle = ->(i){ (i+1) % 3 == 0 ? 'crackle'  : nil}
-    pop     = ->(i){ (i+1) % 5 == 0 ? 'pop'      : nil}
-    (1..100).swap(crackle).append_or_swap(pop)
+    #taking advantage of some special syntax & duck typing that lets me use a block like a dynamically computed hash
+    crackle = ->((i,v)){[i, i % 3 == 0 ? 'crackle' : v ] }
+    pop     = ->((i,v)){ i % 5 == 0 ? (v.add_or_return 'pop') : v }
+    (1..100).zip(1..100).swap(crackle).swap(pop)
 end
 
 puts cracklepop
